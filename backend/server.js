097a372cf2aabc1c58ccaf8133b76db45735a2e9
Hangácsi Gábor <gacsi001@gmail.com>
2022-03-26 17:06:01 +0100

    Introducing routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const dotenv = require('dotenv').config();
+const express = require("express");
+const dotenv = require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
@@ -8,5 +8,7 @@ const app = express();
 app.get("/", (rew, res) => {
   res.status(200).json({ message: "Welcome to the MERN ticketing API" });
 });
+
+app.use("/api/users", require("./routes/userRoutes"));
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
