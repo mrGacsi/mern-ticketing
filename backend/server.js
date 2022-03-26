6536303ac2ebd88a9745d642584f7296a9932469
Hangácsi Gábor <gacsi001@gmail.com>
2022-03-26 16:56:13 +0100

    Init express server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('Server...')
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (rew, res) => {
+  res.status(200).json({ message: "Welcome to the MERN ticketing API" });
+});
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
