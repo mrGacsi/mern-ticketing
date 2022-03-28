2be611298d48f2035e56ab8a0eadb6ed658f9e21
Hangácsi Gábor <gacsi001@gmail.com>
2022-03-28 17:50:14 +0200

    Introducing error handling, user model, and user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,60 @@
-const registerUser = (req, res)=> {
-  res.send('Register route')
-}
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const { use } = require("express/lib/router");
+const { default: mongoose } = require("mongoose");
+const User = require("../models/userModel");
 
-const loginUser = (req, res)=> {
-  res.send('Login route')
-}
+// @desc Register a new user
+// @route /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // Validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please include all fields");
+  }
+
+  // Find if user already exists
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+// @desc Login an existing user
+// @route /api/users/login
+// @access Public
+const loginUser = asyncHandler(async (req, res) => {
+  res.send("Login route");
+});
 
 module.exports = {
   registerUser,
   loginUser,
-}
+};
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,24 @@
-const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
-
+const express = require("express");
+const connectDB = require("./config/db");
+const { errorHandler } = require("./middleware/errorMiddleware");
 const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
 
 const app = express();
 
-app.get("/", (rew, res) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to the MERN ticketing API" });
 });
 
 app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
