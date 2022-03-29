d9a3ca93f2077a4e275ffe5ab9916f89003610a4
Hangácsi Gábor <gacsi001@gmail.com>
2022-03-29 21:16:24 +0200

    Introducing json web token and protected routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,7 @@
-const bcrypt = require("bcryptjs");
 const asyncHandler = require("express-async-handler");
-const { use } = require("express/lib/router");
-const { default: mongoose } = require("mongoose");
+const bcrypt = require("bcryptjs");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
 // @desc Register a new user
 // @route /api/users
@@ -36,10 +35,11 @@ const registerUser = asyncHandler(async (req, res) => {
   });
 
   if (user) {
-    res.status(200).json({
+    res.status(201).json({
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -51,10 +51,46 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route /api/users/login
 // @access Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.send("Login route");
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid credentials");
+  }
 });
+
+// @desc Get current user
+// @route /api/users/me
+// @access Private
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  }
+  res.status(200).json(user);
+});
+
+
+
+// Generate jwt
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = {
   registerUser,
   loginUser,
+  getMe
 };
